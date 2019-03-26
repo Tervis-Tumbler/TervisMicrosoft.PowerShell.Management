fc3e00f6d363a3326f5924a8798f0b8d2d3b8e58
Chris Magnuson <hourglasssand@gmail.com>
2019-03-26 17:30:14 -0400

    Add Disable-InternetExplorerESC

--- TervisMicrosoft.PowerShell.Management.psm1
+++ TervisMicrosoft.PowerShell.Management.psm1
@@ -295,3 +295,16 @@ function ConvertFrom-RemotePath {
         $Path -replace "\\\\[^\\]+\\\w\$","$($PathRoot):"
     }
 }
+
+function Disable-InternetExplorerESC {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+        $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+        Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+        Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0    
+    }
+}
